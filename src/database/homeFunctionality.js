baf01a87cdbadff0c530df474a9ef22e33637f28
ReligaP <ReligaP@interia.pl>
2021-11-28 11:37:07 +0100

    Add homeFunctionality.js and homeInterestingFacts.js to database

---
+++ src/database/homeFunctionality.js
@@ -0,0 +1,5 @@
+const homeFunctionality = ["Możliwość sprawdzenia danych meteorologicznych pochodzących od kilku niezależnych serwisów " +
+"pogodowych takich jak OpenWeather,Windy czy IMGW.","Możliwość sprawdzenia danych hydrologicznych udostępnianych " +
+"przez IMGW (m.in. ponad 45 rzek).","Możliwość dodawania relacji z wędkowania dla zarejestrowanych użytkowników."]
+
+export default homeFunctionality;
