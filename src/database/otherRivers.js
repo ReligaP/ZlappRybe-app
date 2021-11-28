77a8e9cd2848d2489a8ca6214e1c903d8732d438
ReligaP <ReligaP@interia.pl>
2021-11-28 11:58:07 +0100

    Add otherRivers.js and riversBig10.js to database

---
+++ src/database/otherRivers.js
@@ -0,0 +1,6 @@
+const otherRivers= ["Bóbr","Łyna","Nysa Łużycka","Wkra","Brda","Prosna","Drwęca","Wisłok","Wda","Drawa",
+    "Nysa Kłodzka","Poprad","Pasłęka","Rega","Bzura","Wisłoka","Obra","Biebrza","Wierzyca","Nida","Gwda","Liwiec","Orzyc"
+    ,"Wieprza","Barycz","Parsęta","Słupia","Kamienna","Ner","Mała Panew","Raba","Omulew","Kwisa","Ina","Krzna","Wełna"
+    ,"Radomka","Ełk"]
+
+export default otherRivers;
---
+++ src/database/riversBig10.js
@@ -0,0 +1,3 @@
+const riversBig10 = ["Wisła","Odra","Warta","Bug","Narew","San","Noteć","Pilica","Wieprz","Dunajec"];
+
+export default riversBig10;
