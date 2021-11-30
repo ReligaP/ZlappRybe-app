b76b209e274904f4e79f5a89a96e8e950c54257e
ReligaP <ReligaP@interia.pl>
2021-11-30 21:09:29 +0100

    Add changes to Footer.js component

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,20 +1,15 @@
 import React from 'react';
-import {Typography} from "@mui/material";
+import {Box, Typography} from "@mui/material";
 
 const Footer = () => {
     return (
         <>
-            <div className="footer_box">
-                <Typography
-                    variant="h6"
-                    color="primary-text"
-                >
-                    &copy;
-                    <span className="span_footer_1"> Złapp</span>
-                    <span className="span_footer_2">Rybę</span>
-                    2021
-                </Typography>
-            </div>
+            <Box className="footerBox">
+                <h4><sup>&copy;</sup></h4>
+                <Typography className="footerBox_title" variant="h5">Złapp</Typography>
+                <Typography className="footerBox_title2" variant="h5">Rybę</Typography>
+                <Typography variant="h5">2021</Typography>
+            </Box>
         </>
     );
 };
