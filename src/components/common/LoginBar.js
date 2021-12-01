abbfa0797b1184ee6ecdf830fb0a765d01d6e0a4
ReligaP <ReligaP@interia.pl>
2021-12-02 00:14:20 +0100

    Add LoginBar.js component and loginBar.scss

---
+++ src/components/common/LoginBar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import app from "../../firebase/firebaseconfig";
+import { getAuth, signOut} from "firebase/auth";
+import {Box, Typography} from "@mui/material";
+import CancelIcon from '@mui/icons-material/Cancel';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+
+
+const LoginBar = (props) => {
+
+    const ClickHandler = () => {
+        const auth=getAuth(app);
+        signOut(auth).then(() => {
+            console.log("Wylogowany");
+        }).catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+        })
+    }
+
+    return (
+        <Box className="loginBarBox">
+            {props.email ?
+                (<Box className="loginBarBox_logged">
+                    <Typography variant="body">Witaj, {props.email}</Typography>
+                    <CheckCircleIcon className="loginBarBox_logged__icon" fontSize="xsmall"/>
+                    <Button size="small" onClick={ClickHandler}> &nbsp;Wyloguj się</Button>
+                </Box>)
+                :
+                (<Box className="loginBarBox_unlogged">
+                    <Typography variant="body2">Użytkownik niezalogowany&nbsp;</Typography>
+                    <CancelIcon className="loginBarBox_unlogged__icon" fontSize="xsmall"/>
+                </Box>)
+            }
+        </Box>
+    );
+};
+
+export default LoginBar;
