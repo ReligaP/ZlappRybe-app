87357ebfd8bd032be11657804f2b349ce08f220b
ReligaP <ReligaP@interia.pl>
2021-12-02 00:09:55 +0100

    Add changes to Weather.js component and weather.scss

--- src/components/pages/Weather.js
+++ src/components/pages/Weather.js
@@ -1,11 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import {MapContainer,TileLayer} from 'react-leaflet';
-import {Typography} from "@mui/material";
-import Button from '@mui/material/Button';
+import {Box,Typography} from "@mui/material";
 import OpenWeatherApiKey from "../../database/openWeatherApiKey";
 
 
 const Weather = () => {
+
     const [city,setCity] = useState("Sandomierz");
     const [temp,setTemp] = useState([]);
     const [wind,setWind] =useState([]);
@@ -24,22 +24,24 @@ const Weather = () => {
             .then(res => res.json())
             .then(res => setWind(res.wind))
             .catch(err => console.log(err))
-    },[city])
+    },[city]);
     useEffect(() => {
         fetch(API)
             .then(res => res.json())
             .then(res => setClouds(res.clouds))
             .catch(err => console.log(err))
     },[city]);
+
+
     return (
         <>
-            <div className="weatherBox">
-                <div className="weatherBox_title">
+            <Box className="weatherBox">
+                <Box className="weatherBox_title">
                     <Typography variant="h6" align="center">
                         Dane Meteorologiczne
                     </Typography>
-                </div>
-                <div className="weatherBox_map">
+                </Box>
+                <Box className="weatherBox_map">
                     <MapContainer
                         className="weatherBox_map__content"
                         center={[52.06898,19.47997]}
@@ -51,60 +53,68 @@ const Weather = () => {
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
                     </MapContainer>
-                </div>
-                <div className="weatherBox_searchBox">
-                    <input
-                        className="weatherBox_searchBox__input"
-                        onBlur={(e) => setCity(e.target.value)}
-                        type="text"
-                        placeholder="Miasto"
-                    />
-                    <Button>Szukaj</Button>
-                </div>
-                {(temp !== undefined && wind !== undefined && clouds !== undefined) ? (<div className="weatherBox_data">
-                    <Typography variant="h6"  gutterBottom="true">
-                        Aktualna sytuacja pogodowa:
-                    </Typography>
-                    <Typography variant="h6" align="left" gutterBottom="true">
-                        Miejscowość: {city[0].toUpperCase() + city.slice(1)}
-                    </Typography>
-                    <div className="weatherBox_data__items">
-                        <Typography variant="body2"  gutterBottom="true">
-                            Temperatura: <span className="data_item">{temp.temp} &ordm;C</span>
+                </Box>
+                <Box className="weatherBox_searchBox">
+                    <form style={{margin:"25px 0",width:"100%",display:"flex",flexDirection:"column",justifyContent:"center" ,alignItems:"center"}}>
+                        <Typography variant="h6">
+                            Twoja miejscowość:
                         </Typography>
-                        <Typography variant="body2" gutterBottom="true">
-                            Cisnienie: <span className="data_item">{temp.pressure} hpa</span>
+                        <input
+                            className="weatherBox_searchBox__input"
+                            onChange={(e) => setCity(e.target.value)}
+                            type="text"
+                            placeholder="Miasto"
+                            name="city"
+                        />
+                    </form>
+                </Box>
+                {(temp !== undefined && wind !== undefined && clouds !== undefined) ? (<Box className="weatherBox_data">
+                        <Typography variant="h6" >
+                            Aktualna sytuacja pogodowa:
                         </Typography>
-                        <Typography variant="body2" gutterBottom="true">
-                            Wilgotność: <span className="data_item">{temp.humidity} %</span>
+                        <Typography variant="h6" align="left" gutterBottom={true}>
+                            Miejscowość: {city}
                         </Typography>
-                        <Typography variant="body2" gutterBottom="true">
-                            Zachmurzenie: <span className="data_item">{clouds.all} %</span>
+                        <Box className="weatherBox_data__items">
+                            <Typography variant="body2"  gutterBottom={true}>
+                                Temperatura: <span className="data_item">{temp.temp} &ordm;C</span>
+                            </Typography>
+                            <Typography variant="body2" gutterBottom={true}>
+                                Cisnienie: <span className="data_item">{temp.pressure} hpa</span>
+                            </Typography>
+                            <Typography variant="body2" gutterBottom={true}>
+                                Wilgotność: <span className="data_item">{temp.humidity} %</span>
+                            </Typography>
+                            <Typography variant="body2" gutterBottom={true}>
+                                Zachmurzenie: <span className="data_item">{clouds.all} %</span>
+                            </Typography>
+                            <Typography variant="body2" gutterBottom={true}>
+                                Temperatura min: <span className="data_item">{temp.temp_min} &ordm;C</span>
+                            </Typography>
+                            <Typography variant="body2" gutterBottom={true}>
+                                Temperatura max: <span className="data_item">{temp.temp_max} &ordm;C</span>
+                            </Typography>
+                            <Typography variant="body2" gutterBottom={true}>
+                                Predkość wiatru: <span className="data_item">{wind.speed} m/s</span>
+                            </Typography>
+                            <Typography variant="body2" gutterBottom={true}>
+                                Predkość wiatru porywy: <span className="data_item">{wind.gust} m/s</span>
+                            </Typography>
+                            <Typography variant="body2" gutterBottom={true}>
+                                Temperatura odczuwalna: <span className="data_item">{temp.feels_like} &ordm;C</span>
+                            </Typography>
+                        </Box>
+                    </Box>)
+                    :
+                    (<Box className="weatherBox_data__noItems">
+                        <Typography variant="subtitle2">
+                            Niestety nie możemy znaleźć tej miejscowości
                         </Typography>
-                        <Typography variant="body2" gutterBottom="true">
-                            Temperatura min: <span className="data_item">{temp.temp_min} &ordm;C</span>
-                        </Typography>
-                        <Typography variant="body2" gutterBottom="true">
-                            Temperatura max: <span className="data_item">{temp.temp_max} &ordm;C</span>
-                        </Typography>
-                        <Typography variant="body2" gutterBottom="true">
-                            Predkość wiatru: <span className="data_item">{wind.speed} m/s</span>
-                        </Typography>
-                        <Typography variant="body2" gutterBottom="true">
-                            Predkość wiatru porywy: <span className="data_item">{wind.gust} m/s</span>
-                        </Typography>
-                        <Typography variant="body2" gutterBottom="true">
-                            Temperatura odczuwalna: <span className="data_item">{temp.feels_like} &ordm;C</span>
-                        </Typography>
-                    </div>
-                </div>) :(<div className="weatherBox_data__noItems">
-                    <Typography variant="subtitle2">
-                        Niestety nie możemy znaleźć tej miejscowości
-                    </Typography>
-                </div>)
+                    </Box>)
                 }
-            </div>
+            </Box>
         </>
+
     );
 };
 
