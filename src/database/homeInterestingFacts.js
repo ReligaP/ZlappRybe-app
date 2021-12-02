eeee98ec8421e8b1e9df7eedc7b92313cd285e9a
ReligaP <ReligaP@interia.pl>
2021-12-02 01:23:15 +0100

    Add changes to homeInterestingFacts.js and README.md

--- src/database/homeInterestingFacts.js
+++ src/database/homeInterestingFacts.js
@@ -1,6 +1,7 @@
-const homeInterestingFacts = ["W Polsce jest 600 tys. członków Polskiego Związku Wedkarskiego,skupionych w 2700 kołach," +
-"objętych" + " działaniem 47 okręgów.","Długość wszystkich rzek w Polsce wynosi 3768 km.","Najdłuższą rzeką w Polsce jest Wisła " +
-"(1047km długości) a najkrótszą Klonownica (woj.podlaskie) licząca według różnych źródeł od 500 do 800 metrów długości."
-    ,"Największa amplituda wahań stanów wody mają rzeki górskie (np.Dunajec 9-10m)."
-]
+const homeInterestingFacts =
+    ["W Polsce jest 600 tys. członków Polskiego Związku Wedkarskiego,skupionych w 2700 kołach,objętych działaniem 47 okręgów."
+        ,"Długość sieci śródlądowych dróg wodnych w 2020 roku wynosiła 3768 km.","Najdłuższą rzeką w Polsce jest Wisła " +
+    "(1047km długości) a najkrótszą Klonownica (woj.podlaskie) licząca według różnych źródeł od 500 do 800 metrów długości."
+        ,"Największa amplituda wahań stanów wody mają rzeki górskie (np.Dunajec 9-10m)."
+    ]
 export default homeInterestingFacts;
